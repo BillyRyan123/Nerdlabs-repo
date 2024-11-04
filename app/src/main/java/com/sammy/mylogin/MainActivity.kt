86a85cfebe6aac05a000e517b938fbc3648df38d
Your Name <you@example.com>
2024-11-04 20:10:31 +0000

    Added icons and restructured the composables well

--- app/src/main/java/com/sammy/mylogin/MainActivity.kt
+++ app/src/main/java/com/sammy/mylogin/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.sammy.mylogin
 
+import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -27,10 +35,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,21 +67,32 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyLogin(){// Start of Function
     Column (
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White),
        // verticalArrangement= Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){//Start of column
+
         Box(
             modifier= Modifier
                 .align(Alignment.End)
                 .padding(32.dp)
         ){// start of this box
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { /*TODO*/ },
+                modifier=Modifier
+                    .border(
+                        border = BorderStroke(1.dp, Color.Gray), // Outline border color and thickness
+                        shape = MaterialTheme.shapes.medium
+                    ),
+                elevation = null,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFFFFF))
+                ) {
               Text(text = "English",
                   color=Color.Black
 
               )
-                Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "",
+                    tint = Color.Black)
             }
             DropdownMenu(expanded = false , onDismissRequest = { /*TODO*/ }) {
                 DropdownMenuItem(
@@ -84,61 +106,125 @@ fun MyLogin(){// Start of Function
                 )
             }
         }//end of this box
-        Column(
-            modifier= Modifier.fillMaxSize(),
+
+         Column(
+            modifier=Modifier.background(Color.White)
+            .fillMaxSize(),
             verticalArrangement= Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
 
-            // Spacer(modifier = Modifier.height(128.dp))
+        ){
             Text(text = "Login",
-                style= TextStyle(fontSize = 20.sp, fontWeight= FontWeight.Bold)
+                style= TextStyle(fontSize = 28.sp, fontWeight= FontWeight.Bold)
             )
-            Text(text = "You must login to your account to continue")
+            Text(text = "You must login to your \n account to continue")
+             Spacer(modifier = Modifier.height(64.dp))
+
             Spacer(modifier = Modifier.height(16.dp))
              NewComp("E-Mail")
              NewComp("Password")
 
+             Spacer(modifier = Modifier.height(16.dp))
+
             Spacer(modifier = Modifier.height(8.dp))
             Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFD700)),
+                shape = RoundedCornerShape(8.dp),
                 modifier = Modifier
-                    .width(302.dp)
-                    .padding(vertical = 8.dp, horizontal = 16.dp)
-                ,
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFD700))
+                    .width(300.dp)
+                    .height(64.dp),
+                contentPadding = PaddingValues(vertical = 4.dp)
             ) {
                 Box(modifier = Modifier.padding(16.dp)) { // Add padding inside the button
                     Text(
-                        text = "login",
-                        color = Color.White // Set text color to white
+                        text = "Login",
+                        color = Color.White ,
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(horizontal = 8.dp)
                     )
                 }
             }
+            Spacer(modifier = Modifier.height(16.dp))
             Row {// Start of row
-                Box {
                     Text(text="Remember me")
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Box{
+                Spacer(modifier = Modifier.height(16 .dp))
                     Text(text="Forgot Password?")
-                }
 
             }// end of row
-
-            Row{
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Google")
-                }
-                Spacer(modifier = Modifier.width(16.dp))
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "FB")
-                }
-            }
-
             Text(text = "or" )
+            Spacer(modifier = Modifier.height(16.dp))
+           Column (
+               verticalArrangement = Arrangement.Bottom,
+               horizontalAlignment = Alignment.CenterHorizontally
+           ){
+               Row{
+                   Button(onClick = { /*TODO*/ },
+                       modifier= Modifier
+                           .width(120.dp)
+                           .border(
+                               border = BorderStroke(
+                                   1.dp,
+                                   Color.Gray
+                               ),
+                               shape = MaterialTheme.shapes.medium
+                           ),
+                       elevation = null,
+                       colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFFFFF))
+
+                       ) {
+                            // Google icon
+                      Row (
+                         modifier= Modifier.fillMaxWidth(),
+                          verticalAlignment = Alignment.CenterVertically,
+                          horizontalArrangement = Arrangement.Center
+                      ){
+                          Icon(painter = painterResource(id = R.drawable.google_icon),
+                              contentDescription = "Google Image",
+                              tint = Color.Unspecified,
+                              modifier = Modifier.size(24.dp)
+                              )
+                      }
+
+                   }
+                   Spacer(modifier = Modifier.width(8.dp))
+                   Button(onClick = { /*TODO*/ },
+                       modifier= Modifier
+                           .width(120.dp)
+                           .border(
+                               border = BorderStroke(
+                                   1.dp,
+                                   Color.Gray
+                               ), // Outline border color and thickness
+                               shape = MaterialTheme.shapes.medium
+                           ),
+                       elevation = null,
+                       colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFFFFF))
+
+                       ) {
+                       Row (
+                           modifier= Modifier.fillMaxWidth(),
+                           verticalAlignment = Alignment.CenterVertically,
+                           horizontalArrangement = Arrangement.Center
+                       ){
+                           Icon(painter = painterResource(id = R.drawable.apple_icon),
+                               contentDescription = "Apple image",
+                               tint = Color.Unspecified,
+                               modifier = Modifier.size(24.dp))
+                       }
+                   }
+               }
+               Spacer(modifier = Modifier.height(16.dp))
+               Column {
+                   Text(text = "Not Registered, Register")
+               }
+               Spacer(modifier = Modifier.height(16.dp))
+           }
 
         }
+
     }//End of column
+    Spacer(modifier = Modifier.height(16.dp))
 
 }//End of function
 
@@ -148,7 +234,12 @@ fun NewComp(title:String){
         Text(text = title,
 
             )
-        OutlinedTextField(value = "", onValueChange = {})
+        OutlinedTextField(value = "", onValueChange = {},
+            label = { Text("Enter email") },
+            modifier=Modifier
+                .width(300.dp)
+                .height(60.dp)
+        )
     }
 }
 
@@ -157,3 +248,5 @@ fun NewComp(title:String){
 fun MyLoginPreview(){
     MyLogin()
 }
+
+
